2f2e136945206ea31811ba847545f2c68bbba3de
chbndrhnns <chbndrhnns@users.noreply.github.com>
2017-09-12 17:09:30 +0200

    Socket: - add UDP support - add IPv6 support

--- tests/SocketTest.py
+++ tests/SocketTest.py
@@ -8,6 +8,7 @@ from mock import patch
 from Socket import Socket, NetcatServerWrapper
 from exc import *
 
+
 class SocketTest(unittest.TestCase):
     def setUp(self):
         pass
@@ -15,7 +16,7 @@ class SocketTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test__check_tcp_port__pass(self):
+    def test__check_port__tcp_pass(self):
         # prepare
         port = 8080
         host = '127.0.0.1'
@@ -24,12 +25,12 @@ class SocketTest(unittest.TestCase):
         # do
         with NetcatServerWrapper((host, port)) as server:
             try:
-                sock.check_tcp_port(host, port)
+                sock.check_port(host, port)
             except Exception as exc:
                 self.fail('Not expected to fail with {}'.format(exc.__repr__()))
 
     @patch('Socket.socket.socket.connect')
-    def test__check_tcp_port__port_closed__exception(self, mock_connect):
+    def test__check_port__tcp_port_closed__exception(self, mock_connect):
         # prepare
         port = 8991
         host = '127.0.0.2'
@@ -38,9 +39,9 @@ class SocketTest(unittest.TestCase):
 
         # do
         with self.assertRaises(TimeoutError):
-            sock.check_tcp_port(host, port)
+            sock.check_port(host, port)
 
-    def test__check_tcp_port__host_invalid__exception(self):
+    def test__check_port__tcp_host_invalid__exception(self):
         # prepare
         port = 8991
         host = '1r27.0.0.2'
@@ -48,9 +49,9 @@ class SocketTest(unittest.TestCase):
 
         # do
         with self.assertRaisesRegexp(DataError, '[Errno 8]'):
-            sock.check_tcp_port(host, port)
+            sock.check_port(host, port)
 
-    def test__check_tcp_port__port_invalid__exception(self):
+    def test__check_port__tcp_port_invalid__exception(self):
         # prepare
         port = 899100
         host = '127.0.0.2'
@@ -59,11 +60,32 @@ class SocketTest(unittest.TestCase):
 
         # do
         with self.assertRaisesRegexp(DataError, 'not supported'):
-            sock.check_tcp_port(host, port)
+            sock.check_port(host, port)
 
+    def test__check_port__tcp_None_port__exception(self):
+        # prepare
+        port = None
+        host = '127.0.0.2'
+        sock = Socket()
+        # mock_connect.side_effect = socket.error('timed out')
+
+        # do
+        with self.assertRaisesRegexp(DataError, 'not supported'):
+            sock.check_port(host, port)
+
+    def test__check_port__tcp_None_host__exception(self):
+        # prepare
+        port = None
+        host = '127.0.0.2'
+        sock = Socket()
+        # mock_connect.side_effect = socket.error('timed out')
+
+        # do
+        with self.assertRaisesRegexp(DataError, 'not supported'):
+            sock.check_port(host, port)
 
     @patch('Socket.socket.socket.connect')
-    def test__check_tcp_port__dns__pass(self, mock_connect):
+    def test__check_port__tcp_dns__pass(self, mock_connect):
         # prepare
         port = 443
         host = 'www.telekom.de'
@@ -72,6 +94,46 @@ class SocketTest(unittest.TestCase):
 
         # do
         try:
-            sock.check_tcp_port(host, port)
+            sock.check_port(host, port)
         except Exception as exc:
             self.fail('Not expected to fail with {}'.format(exc.__repr__()))
+
+    def test__check_port__tcp_ipv6__pass(self):
+        # prepare
+        port = 443
+        host = '::1'
+        sock = Socket()
+        # mock_connect.return_value = 0
+
+        # do
+        try:
+            sock.check_port(host, port)
+        except Exception as exc:
+            self.fail('Not expected to fail with {}'.format(exc.__repr__()))
+
+    def test__check_port__udp_ipv6__pass(self):
+        # prepare
+        port = 443
+        host = '::1'
+        protocol = 'udp'
+        sock = Socket()
+
+        # do
+        try:
+            sock.check_port(host, port, protocol)
+        except Exception as exc:
+            self.fail('Not expected to fail with {}'.format(exc.__repr__()))
+
+    def test__check_port__protocol_invalid__exception(self):
+        # prepare
+        port = 443
+        host = '::1'
+        protocol = 'uap'
+        sock = Socket()
+
+        # do
+        with self.assertRaisesRegexp(DataError, 'UDP and TCP'):
+            sock.check_port(host, port, protocol)
+
+
+
--- vnf-robot/Socket.py
+++ vnf-robot/Socket.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import socket
+from string import lower
 
+import ipaddress
 from nclib import TCPServer
 
 from settings import Settings
@@ -11,51 +13,40 @@ from robotlibcore import DynamicCore
 from version import VERSION
 
 
-class Socket(DynamicCore):
-    """Socket is used to test for the status of TCP and UDP ports."""
+class Layer3Protocol:
+    allowed = {
+        'udp': socket.SOCK_DGRAM,
+        'tcp': socket.SOCK_STREAM
+    }
 
-    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
-    __version__ = VERSION
+    def __init__(self, protocol):
+        if protocol is None:
+            raise DataError('Only UDP and TCP are support protocols; got None.'.format([protocol]))
+        self.type = self.allowed.get(str.lower(protocol), None)
+        if self.type is None:
+            raise DataError('Only UDP and TCP are support protocols; got "".'.format([protocol]))
 
-    def __init__(self):
-        DynamicCore.__init__(self, [])
 
-        logger.info("Importing {}".format(self.__class__))
-        self.settings = Settings()
+class HostAddress:
+    def __init__(self, address=None):
+        error = None
+        try:
+            self.host_address = ipaddress.ip_address(address)
+        except ipaddress.AddressValueError as exc:
+            error = DataError('Cannot parse provided host "{}"'.format(address))
 
-    def run_keyword(self, name, args, kwargs):
-        """
-        Mandatory method for using robot dynamic libraries
+        try:
+            error = None
+            self.host_address = socket.getaddrinfo(address, 80)
+        except socket.gaierror as exc:
+            error = DataError('Cannot parse provided host "{}"'.format(address))
 
-        Returns:
-            None
-        """
-        logger.info("Running keyword '%s' with arguments %s." % (name, args))
-        return self.keywords[name](*args, **kwargs)
+        if error is not None:
+            raise error
 
-    @keyword('${host}:${port}/TCP}')
-    def check_tcp_port(self, host=None, port=None):
-        """
-        Issues an HTTP GET request to the specified url.
-
-        Args:
-            port: Port to be checked
-
-        Returns:
-            None
-
-        """
-        if port < 0 or port > 65535:
-            raise DataError('Port "{}" is not supported.'.format(port))
-
-        with SocketWrapper(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            try:
-                sock.connect((host, port))
-            except socket.error as exc:
-                if 'timed out' in exc.args:
-                    raise TimeoutError()
-                if exc.errno is 8:
-                    raise DataError('Hostname not provided or invalid.')
+    @property
+    def version(self):
+        return [family[0] for family in self.host_address][0]
 
 
 class SocketWrapper:
@@ -85,3 +76,62 @@ class NetcatServerWrapper:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.server.close()
+
+
+class Socket(DynamicCore):
+    """Socket is used to test for the status of TCP and UDP ports."""
+
+    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
+    __version__ = VERSION
+
+    def __init__(self):
+        DynamicCore.__init__(self, [])
+
+        logger.info("Importing {}".format(self.__class__))
+        self.settings = Settings()
+
+    def run_keyword(self, name, args, kwargs):
+        """
+        Mandatory method for using robot dynamic libraries
+
+        Returns:
+            None
+        """
+        logger.info("Running keyword '%s' with arguments %s." % (name, args))
+        return self.keywords[name](*args, **kwargs)
+
+    @keyword('${host}:${port}/${protocol}}')
+    def check_port(self, host=None, port=None, protocol='TCP'):
+        """
+        Issues an HTTP GET request to the specified url.
+
+        Args:
+            host: host or domain name
+            protocol: TCP or UDP
+            port: Port to be checked
+
+        Returns:
+            None
+
+        """
+
+        # verify port
+        if port is None:
+            raise DataError('None port is not supported.'.format(port))
+        if port < 0 or port > 65535:
+            raise DataError('Port "{}" is not supported.'.format(port))
+
+        # verify host
+        address_family = HostAddress(host).version
+
+        # verify protocol
+        proto = Layer3Protocol(protocol).type
+
+        with SocketWrapper(address_family, proto) as sock:
+            try:
+                sock.connect((host, port))
+            except socket.error as exc:
+                if 'timed out' in exc.args:
+                    raise TimeoutError()
+                if exc.errno is 8:
+                    raise DataError('Hostname not provided or invalid.')
